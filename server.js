ef40d6bbbb4667d2cebbab78b31f31bf90bda9aa
07_Itamar_Melamed <nadav8.tch07@gmail.com>
2022-08-15 12:29:20 +0300

    finalize all requests

--- server.js
+++ server.js
@@ -1,40 +1,96 @@
 'use strict';
 
-import {
-    getBooks    as List, 
-    getBookById as Read,
-    createBook  as Create,
-    deleteBook  as Delete, 
-    updateBook  as Update
-} from '/sub.js';
-
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 const port = 3000;
 
 app.use(express.json());
 
+const validateBook = (book) => {
+    const schema = {
+        name: Joi.string().required(),
+        price: Joi.string().required()
+    };
+
+    return Joi.validate(book, schema);
+};
+
+const getBooks = () => {
+    const data = require('./books.json');
+    return data;
+}
+
+const getBookById = (id) => {
+    return getBooks().find((book) => book.id === id);
+}
+
+const createBook = (content) => {
+    const { error } = validateBook(content);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    return {
+        name: content.name,
+        price: content.price,
+        id: getBooks().length + 1
+    };
+}
+
+const deleteBook = (id) => {
+    const book = getBookById(id);
+    if (!book) {
+        return res.status(404).send('The book with the given ID was not found.');
+    }
+    
+    const index = getBooks().indexOf(book);
+    getBooks().forEach((bookFromList) => {
+        if (getBooks().indexOf(bookFromList) > index) {
+            bookFromList.id--;
+        }
+    })
+    getBooks().splice(index, 1);
+
+    return book;
+}
+
+const updateBook = (id, content) => {
+    const book = getBookById(id);
+    if (!book) {
+        return res.status(404).send('The book with the given ID was not found.');
+    }
+
+    const { error } = validateBook(content);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    book.name = content.name;
+    book.price = content.price;
+}
+
 app.get('/api/books', (req, res) => {
-    res.send(List());
+    res.send(getBooks());
 })
 
 app.get('/api/books/:id', (req, res) => {
-    res.send(Read(parseInt(req.params.id)));
+    res.send(getBookById(parseInt(req.params.id)));
 })
 
 app.post('/api/books', (req, res) => {
-    const newBook = Create(req.body);
+    const newBook = createBook(req.body);
     getBooks().push(newBook);
     res.send(newBook);
 });
 
 app.delete('/api/books/:id', (req, res) => {
-    const deletedBook = Delete(parseInt(req.params.id));
+    const deletedBook = deleteBook(parseInt(req.params.id));
     res.send(deletedBook);
 });
 
 app.put('/api/books/:id', (req, res) => {
-    const updatedBook = Update(parseInt(req.params.id), req.body);
+    const updatedBook = updateBook(parseInt(req.params.id), req.body);
     res.send(updatedBook);
 });
 
